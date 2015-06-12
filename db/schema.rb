5f1fb441c280308e55faa94f318e05cc05cf25df
Taylor Huston <taylor.a.huston@gmail.com>
2015-06-12 14:26:28 -0700

    Create Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612201508) do
+ActiveRecord::Schema.define(version: 20150612212527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email"
+    t.string   "hashed_password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
