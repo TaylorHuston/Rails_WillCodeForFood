d76220e24604f17cce3b1374ce76506633297a2f
Taylor Huston <taylor.a.huston@gmail.com>
2015-06-16 22:02:21 -0700

    Implement editing user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(user_params)
+        redirect_to(:controller => "users", :action => "show", :id => @user.id)
+    else
+      render 'edit'
+    end
+    
   end
 
   def delete
