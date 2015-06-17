fbc8cf2989f8c7f75d3c15611eca8c5d36d4dbb0
Taylor Huston <taylor.a.huston@gmail.com>
2015-06-16 19:37:35 -0700

    Add index to user ID in jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616054348) do
+ActiveRecord::Schema.define(version: 20150617023051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150616054348) do
     t.string   "location",    limit: 50
     t.integer  "user_id"
   end
+
+  add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                 null: false
