e49f95546ea2e202442aa96122b0a1b434fb33b2
Taylor Huston <taylor.a.huston@gmail.com>
2015-06-18 18:54:22 -0700

    Start setting up job postings to be editable

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,6 +21,22 @@ class JobsController < ApplicationController
       end
   end
   
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update_attributes(job_params)
+      flash[:notice] = "Update Successfull"
+#      redirect_to(:controller => "users", :action => "show", :id => @user.id)
+    else
+      render 'edit'
+    end
+  end
+
+
   private
   def job_params
     params.require(:job).permit(:title, :company, :url, :category, :description, :location)
