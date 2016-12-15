47ea41fadaa25ace2721b04f2905cc55eb78a0b9
Taylor Huston <taylor.a.huston@gmail.com>
2016-12-14 20:12:19 -0800

    Add failing tests for missing form information

--- spec/features/creating_jobs_spec.rb
+++ spec/features/creating_jobs_spec.rb
@@ -1,19 +1,82 @@
 require "rails_helper"
 
 RSpec.feature "Anonymous users can create a job posting" do
-  scenario "with valid attributes" do
-    visit "/"
     
-    click_link "Post A Job"
-    fill_in "Title", with: "Test Job"
-    fill_in "Company", with: "Test Company"
-    fill_in "Url", with: "www.google.com"
-    fill_in "Location", with: "Seattle"
-    choose("Job")
-    fill_in "Description", with: "Description text"
-    click_button "Submit Job"
+    before do
+        visit "/"
+        
+        click_link "Post A Job"
+    end
+  
+    scenario "with valid attributes" do
+        fill_in "Title", with: "Test Job"
+        fill_in "Company", with: "Test Company"
+        fill_in "Url", with: "www.google.com"
+        fill_in "Location", with: "Seattle"
+        choose("Job")
+        fill_in "Description", with: "Description text"
+        click_button "Submit Job"
+
+        expect(page).to have_content "Creation Successful"
+    end
     
-    expect(page).to have_content "Creation Successful"
+    scenario "with no title" do
+      
+        fill_in "Company", with: "Test Company"
+        fill_in "Url", with: "www.google.com"
+        fill_in "Location", with: "Seattle"
+        choose("Job")
+        fill_in "Description", with: "Description text"
+        click_button "Submit Job"
+
+        expect(page).to have_content "prohibited this record from being saved"
     
-  end
+    end
+    
+    scenario "with no company" do
+      
+        fill_in "Title", with: "Test Job"
+        fill_in "Url", with: "www.google.com"
+        fill_in "Location", with: "Seattle"
+        choose("Job")
+        fill_in "Description", with: "Description text"
+        click_button "Submit Job"
+
+        expect(page).to have_content "prohibited this record from being saved"
+    
+    end
+        
+    scenario "with no url" do
+        fill_in "Title", with: "Test Job"
+        fill_in "Company", with: "Test Company"
+        fill_in "Location", with: "Seattle"
+        choose("Job")
+        fill_in "Description", with: "Description text"
+        click_button "Submit Job"
+
+        expect(page).to have_content "prohibited this record from being saved"
+    end
+    
+    scenario "with no location" do
+        fill_in "Title", with: "Test Job"
+        fill_in "Company", with: "Test Company"
+        fill_in "Url", with: "www.google.com"
+        choose("Job")
+        fill_in "Description", with: "Description text"
+        click_button "Submit Job"
+
+        expect(page).to have_content "prohibited this record from being saved"
+    end
+    
+    scenario "with no description" do
+        fill_in "Title", with: "Test Job"
+        fill_in "Company", with: "Test Company"
+        fill_in "Url", with: "www.google.com"
+        fill_in "Location", with: "Seattle"
+        choose("Job")
+        click_button "Submit Job"
+
+        expect(page).to have_content "prohibited this record from being saved"
+    end
+    
 end
---
+++ vendor/cache/ruby/2.3.0/gems/recaptcha-4.0.0/lib/recaptcha/version.rb
@@ -0,0 +1,3 @@
+module Recaptcha
+  VERSION = "4.0.0".freeze
+end
