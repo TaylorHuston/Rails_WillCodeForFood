9f31fbb514c9e200670dc9d9a8be4945af0d69e9
Taylor Huston <taylor.a.huston@gmail.com>
2016-12-15 00:35:14 -0800

    Add test for duplicate email

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -18,6 +18,37 @@ RSpec.feature "New users can register" do
     expect(page).to have_content "Registration Successful"
 
   end
+  
+  scenario "with duplicate email" do
+    visit "/"
+    
+    click_link "Login"
+    
+    click_link "Sign Up Here"
+    
+    fill_in "Name", with: "Test Name"
+    fill_in "Email", with: "test2@gmail.com"
+    fill_in "Password", with: "!234AbCd"
+    fill_in "Password confirmation", with: "!234AbCd"
+    
+    click_button "Register"
+    
+    visit "/"
+    
+    click_link "Login"
+    
+    click_link "Sign Up Here"
+    
+    fill_in "Name", with: "Test Name"
+    fill_in "Email", with: "test2@gmail.com"
+    fill_in "Password", with: "!234AbCd"
+    fill_in "Password confirmation", with: "!234AbCd"
+    
+    click_button "Register"
+    
+    expect(page).to have_content "Email has already been taken"
+
+  end
 end
 
     
